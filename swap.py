2b56bb091f366fae89430422d90eeadd4617c07b
dharora <93426753+dharora@users.noreply.github.com>
2023-06-27 10:59:16 -0700

    Delete swap.py

--- swap.py
+++
@@ -1,7 +0,0 @@
-x=int(input("Enter first number:"))
-y=int(input("Enter second number:"))
-x = x + y
-y = x - y
-x = x - y
-print("The first number after swapping is:",x)
-print("The second number after swapping is:",y)
